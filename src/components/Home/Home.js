04f362cd158de6690c6f21115c2e1e3eca7cef7a
joniTalukder <jonictg7@gmail.com>
2022-04-11 11:25:36 +0600

    home page done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import HomeReview from '../HomeReview/HomeReview';
 import './Home.css';
 
 const Home = (props) => {
-    const [reviews, setReviews] = useReviews();
+    const [reviews] = useReviews();
     return (
         <div>
             <div className='header flex'>
@@ -13,7 +13,7 @@ const Home = (props) => {
                     <p className='text-xl'>Here is lots of reviews and have new chart. When you learn need to know lots of thing.</p>
                 </div>
                 <img className='header-img' src="https://hips.hearstapps.com/ghk.h-cdn.co/assets/16/08/gettyimages-464163411.jpg?crop=1.0xw:1xh;center,top&resize=980:*" alt="" />
-                
+
             </div>
             <h2 className='text-4xl m-10'>Customer Reviews(3)</h2>
             <div>
@@ -24,7 +24,7 @@ const Home = (props) => {
                     ></HomeReview>)
                 }
 
-                
+
             </div>
             <button className='bg-indigo-500 font-bold text-white p-6 m-10 rounded-xl'>See All Reviews</button>
         </div>
